644030735ade8effa0aecbcc3db11f5ed8e53fc7
ashraf <chelkatrao@gmail.com>
2020-02-01 16:37:22 +0500

    add some change

--- src/main/java/com/company/quiz/controller/quiz/QuestionController.java
+++ src/main/java/com/company/quiz/controller/quiz/QuestionController.java
@@ -2,6 +2,7 @@ package com.company.quiz.controller.quiz;
 
 import com.company.quiz.dto.quiz.QuestionDto;
 import com.company.quiz.service.quiz.QuestionService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +18,13 @@ public class QuestionController {
     }
 
     @PostMapping("/new")
+    @PreAuthorize("hasAnyAuthority('SUPER_ADMIN_WRITE')")
     public QuestionDto createQuestion(@RequestBody QuestionDto questionDto) {
         return questionService.createQuestion(questionDto);
     }
 
     @GetMapping("/list")
+    @PreAuthorize("hasAnyAuthority('SUPER_ADMIN_READ')")
     public List<QuestionDto> listQuestion() {
         return questionService.listQuestion();
     }
